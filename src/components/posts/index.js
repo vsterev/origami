2c8854e5608f56db47beebb56f743b72d9971aec
vasko <vsterev@gmail.com>
2020-07-13 11:48:01 +0300

    post component linked to API

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import './styles.css'
 import Post from '../post'
-const Posts = () => {
-    return (
-        <div className="Posts">
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-        </div>
-    )
+class Posts extends React.Component {
+    state = {
+        origamis: []
+    }
+    // getPosts = async () => {
+    //     const origamis = await fetch('http://localhost:9999/api/origami').then(res => res.json())
+    //     this.setState({ origamis })
+    // }
+    getPosts() {
+        fetch('http://localhost:9999/api/origami')
+            .then(res => res.json())
+            .then(origamis => this.setState({ origamis }))
+            .catch(err => console.log(err))
+    }
+    componentDidMount() {
+        this.getPosts()
+    }
+    renderOrigami() {
+        return this.state.origamis.map(
+            (origam, id) => <Post key={id} description={origam.description} author={origam.author.username} />
+        )
+    }
+    render() {
+        console.log(this.state.origamis)
+        return (
+            <div className="Posts">
+                {this.renderOrigami()}
+            </div>
+        )
+    }
 }
+
 export default Posts
